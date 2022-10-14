203bd1bb757f6e68742ca66bd9db8ae72620d488
Pratik_Pandya <Pratik_Pandya@epam.com>
2022-10-14 18:50:19 +0530

    Add project files.

---
+++ EPAM_TAO_API_TESTS/BaseAPITestConfig/BaseAPITestConfigurations.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using EPAM_TAO_CORE_API_TAF.APIHelpers;
+using EPAM_TAO_CORE_UI_TAF.UI_Helpers;
+using System.Reflection;
+
+namespace EPAM_TAO_API_TESTS.BaseAPITestConfig
+{
+    [TestClass]
+    public abstract class BaseAPITestConfigurations
+    {
+        public TestContext TestContext { get; set; }
+        public Exception testEx { get; set; }
+        public string strAUT { get { return ConfigurationManager.AppSettings["AUT"].ToString(); } }
+        public string strBaseURL { get { return ConfigurationManager.AppSettings["BaseURL"].ToString(); } }        
+
+        public RestClient restClient;        
+
+        [TestInitialize]
+        public void Setup()
+        {
+            try
+            {
+                restClient = RestClientSetup.restClientSetup.SetupClient(strBaseURL);
+                ExtentReportHelper.GetInstance(strAUT).CreateTest(TestContext.TestName);
+            }
+            catch (Exception ex)
+            {
+                ExtentReportHelper.GetInstance(strAUT).SetTestStatusFail($"<br>{ex.Message}<br>Stack Trace: <br>{ex.StackTrace}<br>");
+            }
+        }
+
+        [TestCleanup]
+        public void Close()
+        {
+            try
+            {
+                var status = TestContext.CurrentTestOutcome;                
+
+                switch (status)
+                {
+                    case UnitTestOutcome.Failed:
+                        var errorMessage = "<pre>" + testEx.Message + "</pre>";
+                        var stacktrace = testEx.StackTrace;                        
+                        ExtentReportHelper.GetInstance(strAUT).SetTestStatusFail($"<br>{errorMessage}<br>Stack Trace: <br>{stacktrace}<br>");
+                        ErrorLogger.errorLogger.ErrorLog(MethodBase.GetCurrentMethod().Name, testEx);
+                        break;
+                    case UnitTestOutcome.Inconclusive:
+                        ExtentReportHelper.GetInstance(strAUT).SetTestStatusSkipped();
+                        break;
+                    default:
+                        ExtentReportHelper.GetInstance(strAUT).SetTestStatusPass();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                ExtentReportHelper.GetInstance(strAUT).SetTestStatusFail($"<br>{ex.Message}<br>Stack Trace: <br>{ex.StackTrace}<br>");
+            }
+            finally
+            {
+                ExtentReportHelper.GetInstance(strAUT).CloseExtentReport();
+            }
+        }
+    }
+}
